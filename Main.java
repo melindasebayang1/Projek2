f5ee88265dede6ff2ca9aad55aee76e92d2bd19f
melinda <melindasebayang1@gmail.com git config --global user.name>
2026-03-13 14:49:54 +0700

    first commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Weapon tombak= new Weapon();
+        tombak.name = " Spear of Dragon";
+        tombak.damage = 180;
+        tombak.showWeapon();
+
+    }
+}
+    
+
